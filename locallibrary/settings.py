9746c3bd04f3494f84764cd63c79fd32913dbdbd
nghiaphunng <duynghia135z@gmail.com>
2025-07-22 16:17:19 +0700

    Django Tutorial Part 12: Django web application security

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -154,3 +154,15 @@ TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'templates')]
 DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
+
+# Production security settings
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_REFERRER_POLICY = "no-referrer-when-downgrade"
